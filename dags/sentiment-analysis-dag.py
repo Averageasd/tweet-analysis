d0ab85215e7a9be007e88120f724cbd033e0a5b0
Averageasd <nhuynh9497@gmail.com>
2026-04-10 17:58:52 -0400

    got DAG to work. will write data to postgres for testing.

--- dags/sentiment-analysis-dag.py
+++ dags/sentiment-analysis-dag.py
@@ -1,3 +1,7 @@
+import pendulum
+from airflow.providers.standard.operators.python import PythonOperator
+from airflow.sdk import DAG
+
 import pandas as pd 
 import pycld2 as cld2
 from nltk.tokenize import word_tokenize
@@ -15,3 +19,26 @@ nltk.download('averaged_perceptron_tagger_eng')
 sid_obj = SentimentIntensityAnalyzer()
 lemmatizer = WordNetLemmatizer()
 
+def _clean_tweets():
+    trump_tweets = pd.read_csv("/opt/airflow/data/hashtag_donaldtrump.csv", lineterminator='\n', parse_dates=True)
+    jb_tweets = pd.read_csv("/opt/airflow/data/hashtag_joebiden.csv", lineterminator='\n', parse_dates=True)
+    trump_tweets['presidential_candidate'] = 'trump'
+    jb_tweets['presidential_candidate'] = 'biden'
+    merged_tweets = pd.concat([trump_tweets, jb_tweets])
+    merged_tweets.drop_duplicates(subset=['tweet_id', 'user_id'],inplace=True)
+    merged_tweets['tweet'] = merged_tweets['tweet'].str.replace(r'@\S+|#\S+|https?://\S+|[^a-zA-Z\s]+', ' ', regex=True)
+    merged_tweets['tweet'] = merged_tweets['tweet'].str.replace(r'\s+', ' ', regex=True).str.strip()
+    merged_tweets.dropna(subset=['user_location', 'lat', 'long', 'city', 'country', 'state_code'], inplace=True)
+    return
+
+with DAG(
+    dag_id="analyze_tweet_sentiment",
+    start_date=pendulum.today("UTC"),
+    schedule=None,
+):
+    clean_tweet_task = PythonOperator(
+        task_id="clean_tweets",
+        python_callable=_clean_tweets
+    )
+    
+    
